fdd6749cb4a252a369e09dd1c76d01f063b44d8b
Boris Buliga <boris@d12frosted.io>
2026-01-31 14:10:14 +0200

    chore: release v1.1.0

--- vulpea-ui.el
+++ vulpea-ui.el
@@ -6,7 +6,7 @@
 ;; Author: Boris Buliga <boris@d12frosted.io>
 ;; Maintainer: Boris Buliga <boris@d12frosted.io>
 ;; URL: https://github.com/d12frosted/vulpea-ui
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Package-Requires: ((emacs "29.1") (vulpea "2.0.0") (vui "0.1"))
 ;; Keywords: outlines hypermedia
 
